989c6faa7e7f9f910bd1ee240651d9a145228a77
Najia Afrin <najiaafrin2@gmail.com>
2019-07-13 03:35:29 +0600

    alpha-child function name change

--- wp-content/themes/alpha-child/functions.php
+++ wp-content/themes/alpha-child/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function alpha_child_enqueue_dequeue_assets(){
+function alpha_child_manage_assets(){
     wp_dequeue_style("alpha-style");
     wp_deregister_style("alpha-style");
     wp_dequeue_style("bootstrap");
@@ -10,4 +10,4 @@ function alpha_child_enqueue_dequeue_assets(){
     wp_enqueue_style("parent-style",get_parent_theme_file_uri("/style.css"),array("bootstrap"));
 }
 
-add_action("wp_enqueue_scripts","alpha_child_enqueue_dequeue_assets",11);
+add_action("wp_enqueue_scripts","alpha_child_manage_assets",11);
